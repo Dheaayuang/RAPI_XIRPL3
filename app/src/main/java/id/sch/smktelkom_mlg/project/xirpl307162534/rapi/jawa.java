dbf20a308e7605a2c3926bdd72b7c2792174d8ff
Leovano Adias <aleovano@gmail.com>
2016-11-20 18:19:48 +0700

    BG_Layout

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl307162534/rapi/jawa.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.project.xirpl307162534.rapi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class jawa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jawa);
+    }
+}
