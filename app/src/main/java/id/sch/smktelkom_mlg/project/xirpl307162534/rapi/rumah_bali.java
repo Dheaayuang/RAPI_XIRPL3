6ae5a0323a5311b442f570a6403bbe73b2840804
Dhea <ayudhea59@gmail.com>
2016-11-23 14:56:49 +0700

    Intent Rumah Bali dan Rumah Jawa

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl307162534/rapi/rumah_bali.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.project.xirpl307162534.rapi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class rumah_bali extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rumah_bali);
+    }
+}
